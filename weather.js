b92b917dbc29ce4b0b17dd1dc56d9903c523abf5
shewag-rk <shewagsurya02@gmail.com>
2025-03-21 23:13:24 +0530

    Search-half-completed

---
+++ weather.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('input');
+const form = document.querySelector('form')
+const searchBtn = document.querySelector('button');
+const temp = document.querySelector('.temperature')
+const desc = document.querySelector('.description');
+const city = document.querySelector('.city');
+const date = document.querySelector('.date');
+const apiKey = "ff1868eb3a9aa0ec63931582110b1b39";
+
+document.addEventListener("DOMContentLoaded", () =>{
+  form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    if(input.value.trim() == ""){
+        alert('Please enter city name');
+    }else{
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=ff1868eb3a9aa0ec63931582110b1b39`)
+        .then(response => response.json())
+        .then(data => {console.log(data)
+            city.innerHTML = data.name;
+            temp.innerHTML = Math.floor(data.main.temp-273.15);
+            desc.innerHTML = data.weather[0].description;
+            date.innerHTML = new Date()
+        })
+    }
+})
+})
